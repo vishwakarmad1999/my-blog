8479b1065569bddd8c87c3a5a21cf4fabb275c45
vishwakarmad1999 <vishwakarmad1999@gmail.com>
2019-01-05 20:49:16 +0530

    Added a few templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+# from django.views.generic import ListView
 
-# Create your views here.
+def post_list(request):
+	return render(request, "blog/post_list.html", {})
