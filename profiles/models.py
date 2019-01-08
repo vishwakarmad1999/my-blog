758d4ed89adcd42d7ffd2f150fb915d1d4fa7193
vishwakarmad1999 <vishwakarmad1999@gmail.com>
2019-01-08 14:35:19 +0530

    Appended a -

--- profiles/models.py
+++ profiles/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 	def send_activation_mail(self):
 		send_mail(
 	    'Verify yourself',
-	    'Dear ' + self.user.username + '\nGo to this link: ' + self.request.get_host + '/' + self.activation_key,
+	    'Dear ' + self.user.username + '\nGo to this link: 127.0.0.1:8000/' + self.activation_key,
 	    '',
 	    [self.user.email],
 	    fail_silently=False,
