946111d4ea9333fe925cae683d4bf2baea0b1d50
vishwakarmad1999 <vishwakarmad1999@gmail.com>
2019-01-31 00:14:58 +0530

    Added 404.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wp23y3%d*o7c&&2nua*+mcpmw8%is()c5+_=)h7fybks0mueh&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'spoofed-domain.com', 'localhost']
 
